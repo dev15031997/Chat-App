24f87ad51d689283ed5b8444c108b436474d1d89
dev15031997 <devashishpal15@gmail.com>
2025-06-04 01:36:51 +0530

    schema created for user and message

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv"; 
+import connectDB from "./config/database.js";
 dotenv.config({});
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, ()=>{
+const app=express();
+
+
+app.listen(PORT, ()=>{
     connectDB();
     console.log(`Server listen at prot ${PORT}`);
 });
