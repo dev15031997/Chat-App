e6ea19cc2b031a40073dbc390fd6b8295c540ce2
dev15031997 <devashishpal15@gmail.com>
2025-06-04 01:59:11 +0530

    user register,login and logout business logic

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv"; 
 import connectDB from "./config/database.js";
+import userRoute from "./routes/userRoute.js";
 dotenv.config({});
 
 const PORT = process.env.PORT || 5000;
 
 const app=express();
 
+// routes
+app.use("/api/v1/user",userRoute); 
 
 app.listen(PORT, ()=>{
     connectDB();
