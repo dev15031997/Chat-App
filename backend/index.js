11e56bbcac4af949aad5180a86bee7a5fe614acb
dev15031997 <devashishpal15@gmail.com>
2025-06-05 00:27:01 +0530

    user -other user and authentication

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,19 @@ import express from "express";
 import dotenv from "dotenv"; 
 import connectDB from "./config/database.js";
 import userRoute from "./routes/userRoute.js";
+import cookieParser from "cookie-parser";
 dotenv.config({});
 
 const PORT = process.env.PORT || 5000;
 
 const app=express();
+
+
+// middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json()); 
+app.use(cookieParser());
+
 
 // routes
 app.use("/api/v1/user",userRoute); 
