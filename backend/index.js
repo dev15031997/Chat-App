a266ce7139b54c41aa2edcd445de215d0be16df4
dev15031997 <devashishpal15@gmail.com>
2025-06-05 01:56:42 +0530

    get and send message

--- backend/index.js
+++ backend/index.js
@@ -2,22 +2,21 @@ import express from "express";
 import dotenv from "dotenv"; 
 import connectDB from "./config/database.js";
 import userRoute from "./routes/userRoute.js";
+import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
+
 dotenv.config({});
-
 const PORT = process.env.PORT || 5000;
-
 const app=express();
-
 
 // middleware
 app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 
-
 // routes
 app.use("/api/v1/user",userRoute); 
+app.use("/api/v1/message",messageRoute);
 
 app.listen(PORT, ()=>{
     connectDB();
