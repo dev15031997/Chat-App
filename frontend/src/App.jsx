3ff50110c520ac469cf81b430b95fe15c13aeb29
dev15031997 <devashishpal15@gmail.com>
2025-06-06 00:18:49 +0530

    login and signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
+
 import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";import Signup from './components/Signup';
+import Login from './components/Login';
+import HomePage from './components/HomePage';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<HomePage/>
+  },
+  {
+    path:"/signup",
+    element:<Signup/>
+  },
+  {
+    path:"/login",
+    element:<Login/>
+  },
+
+])
 
 function App() {
 
   return (
-    <>
-        <h1>Hello there</h1>
-    </>
-  )
+     <div className="p-4 h-screen flex items-center justify-center">
+       <RouterProvider router={router}/>
+     </div>
+ 
+   );
 }
 
 export default App
