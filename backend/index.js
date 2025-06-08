8dccbcc7c936a44dd19369803b0ee273a44809cb
dev15031997 <devashishpal15@gmail.com>
2025-06-08 20:55:36 +0530

    login and singup page updated

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import connectDB from "./config/database.js";
 import userRoute from "./routes/userRoute.js";
 import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config({});
 const PORT = process.env.PORT || 5000;
@@ -13,6 +14,11 @@ const app=express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
+const corsOption={
+    origin:'http://localhost:5173',
+    credentials:true
+};
+app.use(cors(corsOption)); 
 
 // routes
 app.use("/api/v1/user",userRoute); 
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const Signup = () => {
   const [user, setUser] = useState({
@@ -9,12 +11,28 @@ const Signup = () => {
     confirmPassword: "",
     gender: "",
   });
-//   const navigate = useNavigate();
+  const navigate = useNavigate();
   const handleCheckbox = (gender) => {
     setUser({ ...user, gender });
   }
   const onSubmitHandler = async (e) => {
     e.preventDefault();
+
+    try {
+          const res = await axios.post(`http://localhost:8080/api/v1/user/register`, user, {
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            withCredentials: true
+          });
+          if (res.data.success) {
+            navigate("/login");
+            toast.success(res.data.message);
+          }
+        } catch (error) {
+          toast.error(error.response.data.message);
+          console.log(error);
+        }
 
     setUser({
       fullName: "",
