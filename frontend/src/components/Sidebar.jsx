6d5c450c51acdfbbf84f69a33a59dfe79285e76f
dev15031997 <devashishpal15@gmail.com>
2025-06-10 00:54:07 +0530

    sidebar search created for user

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,12 +3,14 @@ import { BiSearchAlt2 } from "react-icons/bi";
 import axios from "axios";
 import toast from "react-hot-toast";
 import {useNavigate} from "react-router-dom";
-import {useDispatch} from "react-redux";
-import { setAuthUser } from '../redux/userSlice';
+import {useDispatch,useSelector} from "react-redux";
+import { setAuthUser, setOtherUsers, setSelectedUser  } from '../redux/userSlice';
+import { setMessages } from '../redux/messageSlice';
 import OtherUsers from './OtherUsers';
 
 const Sidebar = () => {
     const [search, setSearch] = useState("");
+    const {otherUsers} = useSelector(store=>store.user);
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
@@ -18,14 +20,27 @@ const Sidebar = () => {
             navigate("/login");
             toast.success(res.data.message);
             dispatch(setAuthUser(null));
+            dispatch(setMessages(null));
+            dispatch(setOtherUsers(null));
+            dispatch(setSelectedUser(null));
         } catch (error) {
             console.log(error);
         }
     }
 
+    const searchSubmitHandler = (e) => {
+        e.preventDefault();
+        const conversationUser = otherUsers?.find((user)=> user.fullName.toLowerCase().includes(search.toLowerCase()));
+        if(conversationUser){
+            dispatch(setOtherUsers([conversationUser]));
+        }else{
+            toast.error("User not found!");
+        }
+    }
+
     return (
         <div className='border-r border-slate-500 p-4 flex flex-col'>
-            <form className='flex items-center gap-2'>
+            <form onSubmit={searchSubmitHandler} action="" className='flex items-center gap-2'>
                 <input
                     value={search}
                     onChange={(e)=>setSearch(e.target.value)}
