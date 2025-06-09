862c62a75ccf107d2c3aa6b281915e9c64f68bb4
dev15031997 <devashishpal15@gmail.com>
2025-06-09 23:49:55 +0530

    send message

--- frontend/src/components/SendInput.jsx
+++ frontend/src/components/SendInput.jsx
@@ -1,13 +1,33 @@
 import React, {useState } from 'react'
 import { IoSend } from "react-icons/io5";
-
+import axios from "axios";
+import {useDispatch,useSelector} from "react-redux";
+import { setMessages } from '../redux/messageSlice';
 
 const SendInput = () => {
     const [message, setMessage] = useState("");
+    const dispatch = useDispatch();
+    const {selectedUser} = useSelector(store=>store.user);
+    const {messages} = useSelector(store=>store.message);
 
+     const onSubmitHandler = async (e) => {
+            e.preventDefault();
+            try {
+                const res = await axios.post(`http://localhost:8080/api/v1/message/send/${selectedUser?._id}`, {message}, {
+                    headers:{
+                        'Content-Type':'application/json'
+                    },
+                    withCredentials:true
+                });
+                dispatch(setMessages([...messages, res?.data?.newMessage]))
+            } catch (error) {
+                console.log(error);
+            } 
+            setMessage("");
+        }
    
     return (
-        <form  className='px-4 my-3'>
+        <form onSubmit={onSubmitHandler} className='px-4 my-3'>
             <div className='w-full relative'>
                 <input
                     value={message}
