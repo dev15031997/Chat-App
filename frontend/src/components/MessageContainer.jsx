cb5088ba8aa737bcace4d3463d13563d73f67538
dev15031997 <devashishpal15@gmail.com>
2025-06-11 00:25:24 +0530

    redux persist setup

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -4,7 +4,8 @@ import Messages from './Messages'
 import { useSelector } from "react-redux";
 
 const MessageContainer = () => {   
-    const { selectedUser, authUser } = useSelector(store => store.user);
+    const { selectedUser, authUser,onlineUsers } = useSelector(store => store.user);
+    const isOnline = onlineUsers?.includes(selectedUser?._id);
 
     return (
         <>
@@ -12,7 +13,7 @@ const MessageContainer = () => {
                 selectedUser !== null ? (
                     <div className='md:min-w-[550px] flex flex-col'>
                         <div className='flex gap-2 items-center bg-zinc-800 text-white px-4 py-2 mb-2'>
-                            <div className='avatar online'>
+                            <div className={`avatar ${isOnline ? 'online' : ''}`}>
                                 <div className='w-12 rounded-full'>
                                     <img src={selectedUser?.profilePhoto} alt="user-profile" />
                                 </div>
